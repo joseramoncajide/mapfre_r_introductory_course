70185ad17f2aeb53cef99972332c885e7bd2b8b1
Jose Ramón Cajide <joseramoncajide@users.noreply.github.com>
2021-09-14 11:10:30 +0200

    shiny

---
+++ S8-02-shiny.R
@@ -0,0 +1,16 @@
+##########################################################################
+# Jose Cajide - @jrcajide
+# Desarrollo de apps en R con Shiny
+##########################################################################
+
+# http://shiny.rstudio.com/images/shiny-cheatsheet.pdf
+
+install.packages("shiny")
+library(shiny)
+
+runExample("01_hello")
+
+
+# Nuestro objetivo: -------------------------------------------------------
+
+runGitHub("curso_introduccion_R", "joseramoncajide", subdir = "app-gapminder")
